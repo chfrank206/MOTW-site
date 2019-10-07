e600a0b055fe40c60701da3964edda56807266f6
Hayden <chfrank9011@gmail.com>
2019-10-07 13:34:04 -0500

    Added components folder and router on App.tsx to direct between them.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,37 +1,27 @@
 import * as React from 'react';
-
-class App extends React.Component<IAppProps, IAppState> {
-
-    constructor(props: IAppProps) {
-        super(props);
-        this.state = {
-            name: null
-        };
-    }
-
-    async componentWillMount() {
-        try {
-            let r = await fetch('/api/hello');
-            let name = await r.json();
-            this.setState({ name });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    render() {
-        return (
-            <main className="container my-5">
-                <h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-            </main>
-        )
-    }
-}
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home';
 
 export interface IAppProps { }
 
-export interface IAppState {
-    name: string;
+const App: React.SFC<IAppProps> = () => {
+
+    
+    
+
+    
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                </Switch>
+            </Router>
+        );
+    
 }
+
+
+
+
 
 export default App;
---
+++ src/client/Components/Home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export interface IHomeProps {
+
+}
+
+const Home: React.SFC<IHomeProps> = () => {
+
+    return (
+        <>
+            <h1>Working!</h1>
+        </>
+    )
+}
+
+
+export default Home;
