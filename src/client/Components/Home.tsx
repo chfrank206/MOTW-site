3a130f2110d9d291a7554dd406d5ebc35a6f751f
Hayden <chfrank9011@gmail.com>
2019-10-09 09:25:23 -0500

    Adding uncommited changes from testerday.

--- src/client/Components/Home.tsx
+++ src/client/Components/Home.tsx
@@ -19,21 +19,39 @@ const Home: React.SFC<IHomeProps> = () => {
 
                 <p className="text-color site-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur vitae nunc sed velit. Vitae justo eget magna fermentum. Nisi vitae suscipit tellus mauris a diam maecenas. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Sit amet est placerat in egestas. Et odio pellentesque diam volutpat commodo sed egestas egestas. Donec massa sapien faucibus et. Habitant morbi tristique senectus et netus et. Egestas dui id ornare arcu. Adipiscing commodo elit at imperdiet dui accumsan sit amet. At lectus urna duis convallis convallis tellus. Sollicitudin nibh sit amet commodo nulla facilisi nullam. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Posuere morbi leo urna molestie at elementum. Venenatis tellus in metus vulputate eu.</p>
 
+                <hr/>
+
                 <img className="image" src="images/bigfoot.jpg" alt="bigfoot"/>
+
+                <hr/>
 
                 <p className="text-color site-font">Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus. Vel turpis nunc eget lorem dolor sed viverra. Massa sapien faucibus et molestie ac. Donec ac odio tempor orci. Tellus cras adipiscing enim eu turpis egestas. Id ornare arcu odio ut. A arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. In iaculis nunc sed augue lacus. Tellus orci ac auctor augue mauris augue neque. Nulla facilisi etiam dignissim diam quis. Nibh cras pulvinar mattis nunc. Quisque non tellus orci ac auctor augue. Iaculis urna id volutpat lacus. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. Suspendisse faucibus interdum posuere lorem ipsum dolor. Adipiscing elit duis tristique sollicitudin. Sit amet tellus cras adipiscing enim eu turpis egestas. Id aliquet risus feugiat in ante metus dictum at tempor.</p>
 
+                <hr/>
+
                 <img className="image" src="images/leggy-cryptid.jpg" alt=""/>
+
+                <hr/>
 
                 <p className="text-color site-font">Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Auctor augue mauris augue neque gravida. Mauris cursus mattis molestie a iaculis. Aliquam purus sit amet luctus venenatis lectus magna. Nullam non nisi est sit amet facilisis. Neque gravida in fermentum et. Adipiscing elit pellentesque habitant morbi. Donec et odio pellentesque diam volutpat. Quis commodo odio aenean sed adipiscing diam donec. Nisi scelerisque eu ultrices vitae. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Sit amet justo donec enim diam vulputate. Amet venenatis urna cursus eget nunc scelerisque viverra. Tincidunt ornare massa eget egestas purus viverra. Eu consequat ac felis donec et odio. Nunc sed augue lacus viverra vitae congue eu consequat. Ut pharetra sit amet aliquam id diam maecenas. Tellus at urna condimentum mattis pellentesque id nibh tortor id.</p>
 
+                <hr/>
+
                 <img className="image" src="images/Mothman.jpg" alt=""/>
 
+                <hr/>
+
                 <p className="text-color site-font">Est sit amet facilisis magna etiam. Enim ut tellus elementum sagittis vitae et leo duis. Donec ultrices tincidunt arcu non sodales neque sodales ut. Tincidunt dui ut ornare lectus sit amet est placerat. Id aliquet risus feugiat in. Lorem ipsum dolor sit amet consectetur adipiscing elit. Bibendum at varius vel pharetra vel turpis nunc eget. Malesuada pellentesque elit eget gravida cum sociis natoque. Risus ultricies tristique nulla aliquet enim. Quam viverra orci sagittis eu volutpat odio. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Montes nascetur ridiculus mus mauris vitae ultricies. Tristique nulla aliquet enim tortor at auctor urna nunc. Suspendisse sed nisi lacus sed viverra tellus in hac. Lorem donec massa sapien faucibus. Eros in cursus turpis massa tincidunt. Bibendum at varius vel pharetra vel. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo.</p>
+
+                <hr/>
                 
                 <img className="image" src="images/ufo.jpg" alt=""/>
 
+                <hr/>
+
                 <p className="text-color site-font">Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Eros donec ac odio tempor orci dapibus ultrices in. Elementum tempus egestas sed sed risus pretium quam vulputate. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Ac tincidunt vitae semper quis lectus nulla at volutpat diam. Faucibus et molestie ac feugiat sed lectus vestibulum. Viverra aliquet eget sit amet. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Pulvinar mattis nunc sed blandit libero volutpat. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc. Lorem donec massa sapien faucibus et. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Porttitor eget dolor morbi non arcu risus quis. Ante metus dictum at tempor commodo.</p>
+                
+                <hr/>
 
                 <img className="image" src="images/ufo2.jpg" alt=""/>
 
